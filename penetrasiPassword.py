fedc63839036d655c116b0c07ce9816b5ba1408a
Azhari Rizkita Priyono <azhari.priyono@gmail.com>
2018-04-16 08:11:56 +0700

    separated function for understanding

--- penetrasiPassword.py
+++ penetrasiPassword.py
@@ -4,28 +4,31 @@ import time
 import sys
 
 
+def matched(a, b):
+    if a == b:
+        print('String matched! Password is : {}'.format(list(a)))
+        sys.exit()
+
+
+def penetrate(real):
+    strings = string.ascii_lowercase
+    for password_length in range(1, len(real) + 1):
+        for temporary in itertools.product(strings, repeat=password_length):
+            temporary = ''.join(temporary)
+            a = temporary
+            b = real
+            matched(a, b)
+            print('Testing : {} || {}'.format(list(temporary), time.perf_counter() - start))
+
+
 def main():
-    passw = input('Set password : ')
-    print('Password telah diset sebagai : ', passw)
+    password = input('Set password : ')
+    print('Password telah diset sebagai : ', password)
     accept = input('Lanjutkan proses penetrasi? (Y/N)')
     if accept == 'Y' or accept == 'y':
         global start
         start = time.perf_counter()
-        print(penetrasiPassword(passw))
-        input('Press enter to terminate program')
-        sys.exit()
-
-
-def penetrasiPassword(real):
-    strings = string.ascii_lowercase
-    percobaan = 0
-    for password_length in range(1, len(real) + 1):
-        for tempPass in itertools.product(strings, repeat=password_length):
-            percobaan += 1
-            tempPass = ''.join(tempPass)
-            if tempPass == real:
-                return 'Password match! Password : {}, ditemukan dalam percobaan ke-{}'.format(list(tempPass), percobaan)
-            print('Testing : ', list(tempPass), ' || Nomor percobaan : ', percobaan, ' || ', time.perf_counter() - start)
+        print(penetrate(password))
 
 
 if __name__ == '__main__':
